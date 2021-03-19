1dc58fc7e60f9292607d034b1def536ab83c86be
TonyLee <tonylee838@foxmail.com>
2021-03-19 19:56:14 +0800

    Add SQL file.

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,15 +1,16 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root",
-    database:"camera"
-})
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "camera",
+  port: 3306,
+});
 
-db.connect( (err) => {
-    if(err) throw err;
-    console.log('数据库连接成功');
-})
+db.connect((err) => {
+  if (err) throw err;
+  console.log("数据库连接成功");
+});
 
-module.exports = db
+module.exports = db;
