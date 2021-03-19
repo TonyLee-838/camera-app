5dbcce9560237249e5bd917bafe35f42645dba05
lyj578 <511581884@qq.com>
2021-03-19 14:06:07 +0800

    ok

---
+++ server/data/ann.py
@@ -0,0 +1,17 @@
+from annoy import AnnoyIndex
+import sys
+
+#将nodejs传递过来的参数转化为数组
+features=sys.argv[1]
+features=features.split(',')
+features=[ float(x) for x in features ]
+
+k=5
+dimensions=500
+indexPath = "./data/index.ann"
+
+annIndex=AnnoyIndex(dimensions, 'angular')
+annIndex.load(indexPath)
+result=annIndex.get_nns_by_vector(features,k,search_k=-1)
+#返回结果给nodejs
+print(result)
